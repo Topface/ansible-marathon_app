5218955138c8d9a8ed6bff27abad7d6d2a5a16d9
Ludovic Claude <ludovic.claude54@gmail.com>
2016-02-25 20:15:47 +0100

    Fix the fix

--- library/marathon_app.py
+++ library/marathon_app.py
@@ -486,11 +486,12 @@ def main():
 
     # Ensure that we use int values for some healthChecks parameters
     if module.params['healthChecks']:
-      checks = module.params['healthChecks']
-      for param in ['port', 'gracePeriodSeconds', 'intervalSeconds', 'timeoutSeconds', 'maxConsecutiveFailures']:
-        if param in checks:
-          checks[param] = int(checks[param])
-      module.params['healthChecks'] = checks
+      healthChecks = module.params['healthChecks']
+      for checks in healthChecks:
+        for param in ['port', 'gracePeriodSeconds', 'intervalSeconds', 'timeoutSeconds', 'maxConsecutiveFailures']:
+          if param in checks:
+            checks[param] = int(checks[param])
+      module.params['healthChecks'] = healthChecks
 
     if module.params['docker_image'] and not module.params['container']:
     	module.params['container'] = { 'type': 'DOCKER', 'docker': { 'image': module.params['docker_image'], 'forcePullImage': bool(module.params['docker_forcePullImage']), 'privileged': bool(module.params['docker_privileged']), 'network': module.params['docker_network'], 'parameters': module.params['docker_parameters'], 'portMappings': module.params['docker_portMappings']}, 'volumes': module.params['container_volumes']}
