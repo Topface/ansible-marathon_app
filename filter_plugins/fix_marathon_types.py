43799c89d0613a6bac5ea82041c90f7e18a6e80b
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-11-08 15:15:06 +0100

    Convert types with more controls

---
+++ filter_plugins/fix_marathon_types.py
@@ -0,0 +1,60 @@
+from functools import partial
+
+def needInt(a):
+    return a in (
+        "/ports[]",
+        "/container/docker/portMappings[]/containerPort",
+        "/container/docker/portMappings[]/hostPort",
+        "/container/docker/portMappings[]/servicePort",
+        "/healthChecks[]/portIndex",
+        "/healthChecks[]/port",
+        "/healthChecks[]/gracePeriodSeconds",
+        "/healthChecks[]/maxConsecutiveFailures",
+        "/healthChecks[]/intervalSeconds",
+        "/healthChecks[]/timeoutSeconds",
+        "/instances")
+
+def needFloat(a):
+    return a in (
+        "disk",
+        "cpus",
+        "mem")
+
+def fix_named_type(name, a):
+
+    if a is None:
+        return a
+
+    forceInt = needInt(name)
+    forceFloat = needFloat(name)
+
+    if forceInt and isinstance(a, (int, long)):
+        return a
+    elif forceFloat and isinstance(a, float):
+        return a
+    elif isinstance(a, dict):
+        casted_dict = dict()
+        for k,v in a.iteritems():
+            casted_dict[k] = fix_named_type(name + "/" + k, v)
+        return casted_dict
+    elif isinstance(a, list):
+        return map(partial(fix_named_type, name + "[]"), a)
+    elif forceInt and isinstance(a, basestring):
+        try:
+            return int(a)
+        except ValueError:
+            return a
+    elif forceFloat and isinstance(a, basestring):
+        try:
+            return float(a)
+        except ValueError:
+            return a
+    else:
+        return a
+
+def fix_marathon_types(a):
+    return fix_named_type("", a)
+
+class FilterModule(object):
+    def filters(self):
+        return {'fix_marathon_types': fix_marathon_types}
